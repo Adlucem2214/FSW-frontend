4a15c21c984bed3be13b79912aeca829ccba6411
iamsamienda <samuelendale7373@gmail.com>
2024-01-28 12:35:11 +0300

    Refactor sellget.js to remove console.log statements

--- src/Fetch/sellget.js
+++ src/Fetch/sellget.js
@@ -12,37 +12,32 @@ fetch('http://localhost:8000/stock/sales/', {
     const tableBody = document.querySelector('#saleTable tbody');
 
     data.forEach(sale => {
-      console.log(sale);
       const row = document.createElement('tr');
 
       const codeCell = document.createElement('td');
       codeCell.textContent = sale.id;
       row.appendChild(codeCell);
-      console.log(codeCell);
 
 
       const productCell = document.createElement('td');
       productCell.textContent = sale.product_str;
       row.appendChild(productCell);
-      console.log(productCell);
+     
 
 
       const quantityCell = document.createElement('td');
       quantityCell.textContent = sale.quantity;
       row.appendChild(quantityCell);
-      console.log(quantityCell);
 
 
       const priceCell = document.createElement('td');
       priceCell.textContent = sale.total_price;
       row.appendChild(priceCell);
-      console.log(priceCell);
 
 
       const dateCell = document.createElement('td');
       dateCell.textContent = sale.date;
       row.appendChild(dateCell);
-      console.log(dateCell);
       
 
     
