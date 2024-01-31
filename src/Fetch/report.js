8bd03472f6d69e796de8e14b9adc920124089f57
iamsamienda <samuelendale7373@gmail.com>
2024-01-31 12:18:26 +0300

    Refactor fetchData and processData functions, add updateGraph function

--- src/Fetch/report.js
+++ src/Fetch/report.js
@@ -1,36 +1,101 @@
-// function downloadTable(version) {
-//     const apiUrl = 'http://localhost:8000/stock/products/'; // Replace with the actual API endpoint that provides the table data
-//     const filename = `table_version_${version}.csv`;
-  
-//     fetch(apiUrl)
-//       .then(response => response.json())
-//       .then(data => {
-//         console.log(data)
-//         // Convert table data into CSV format
-//         let csvContent = '';
-//         data.forEach(row => {
-//           const rowData = Object.values(row).map(value => {
-//             if (typeof value === 'string') {
-//               return value.replace(/,/g, '');
-//             }
-//             return String(value);
-//           });
-//           csvContent += rowData.join(',') + '\n';
-//         });
-  
-//         // Create and download CSV file
-//         const element = document.createElement('a');
-//         element.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvContent);
-//         element.download = filename;
-//         element.style.display = 'none';
-//         document.body.appendChild(element);
-//         element.click();
-//         document.body.removeChild(element);
-//       })
-//       .catch(error => {
-//         console.error('Error:', error);
-//       });
-//   }
+
+function fetchData() {
+  return fetch("http://127.0.0.1:8000/stock/sales/").then((response) =>
+    response.json()
+  );
+}
+
+function processData(data, dateRange) {
+  const currentDate = new Date();
+
+  const aggregatedData = {};
+
+  data.forEach((sale) => {
+    const saleDate = new Date(sale.date);
+
+    let startDate;
+    if (dateRange === "weekly") {
+      startDate = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        currentDate.getDate() - 7
+      );
+    } else if (dateRange === "monthly") {
+      startDate = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() - 1,
+        currentDate.getDate()
+      );
+    } else if (dateRange === "daily") {
+      startDate = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        currentDate.getDate() - 1
+      );
+    } else {
+      throw new Error("Invalid date range");
+    }
+
+    if (saleDate >= startDate && saleDate <= currentDate) {
+      if (!aggregatedData[sale.product_str]) {
+        aggregatedData[sale.product_str] = 0;
+      }
+      if (sale.quantity != 0){
+      aggregatedData[sale.product_str] += sale.quantity;
+    }
+  }
+  });
+
+  const productIds = Object.keys(aggregatedData);
+  const quantitiesSold = Object.values(aggregatedData);
+
+  const trace1 = {
+    x: productIds,
+    y: quantitiesSold,
+    type: "bar",
+    marker: {
+      color: "skyblue",
+    },
+  };
+
+  //  const trace2 = {
+  //   values: quantitiesSold,
+  //   labels: productIds,
+  //   type: "pie",
+  // };
+
+  // console.log(trace2)
+
+
+
+  return [trace1];
+}
+
+function updateGraph(dateRange) {
+  fetchData()
+    .then((data) => {
+      const processedData1 = processData(data, dateRange);
+      // const processedData2 = processData(data, dateRange)[1];
+
+      // console.log(processedData1);
+
+      const graphDiv1 = document.getElementById("graph1");
+      Plotly.newPlot(graphDiv1, processedData1);
+
+      // var layout = {
+      //   height: 1000,
+      //   width: 1000
+      // };
+
+      // const graphDiv2 = document.getElementById("graph2");
+      // Plotly.newPlot(graphDiv2, processedData2, layout);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
+
+updateGraph("weekly");
 
 
 function downloadTable(version) {
