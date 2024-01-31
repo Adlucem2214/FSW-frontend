62a3daeec439224c285d8da274a081bc8ce74b9f
iamsamienda <samuelendale7373@gmail.com>
2024-01-31 13:53:45 +0300

    Fix authorization token in report.js

--- src/Fetch/report.js
+++ src/Fetch/report.js
@@ -5,7 +5,7 @@ function fetchData() {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Token ${token},`,
+      'Authorization': `Token ${token}`,
     },
 }
   ).then((response) =>
@@ -106,58 +106,58 @@ function updateGraph(dateRange) {
 updateGraph("weekly");
 
 
-function downloadTable(version) {
-    const apiUrl = 'http://localhost:8000/stock/products/'; // Replace with the actual API endpoint that provides the table data
-    const filename = `table_version_${version}.csv`;
+// function downloadTable(version) {
+//     const apiUrl = 'http://localhost:8000/stock/products/'; // Replace with the actual API endpoint that provides the table data
+//     const filename = `table_version_${version}.csv`;
   
-  fetch(apiUrl,{
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Token ${token},`,
-    }}
-  )
-      .then(response => response.json())
-      .then(data => {
-        // Extract table headers
-        const headers = Object.keys(data[0]);
+//   fetch(apiUrl,{
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization': `Token ${token},`,
+//     }}
+//   )
+//       .then(response => response.json())
+//       .then(data => {
+//         // Extract table headers
+//         const headers = Object.keys(data[0]);
   
-        // Convert table data into CSV format
-        let csvContent = headers.join(',') + '\n';
-        data.forEach(row => {
-          const rowData = headers.map(header => {
-            const value = row[header];
-            if (typeof value === 'string') {
-              return value.replace(/,/g, '');
-            }
-            return String(value);
-          });
-          csvContent += rowData.join(',') + '\n';
-        });
+//         // Convert table data into CSV format
+//         let csvContent = headers.join(',') + '\n';
+//         data.forEach(row => {
+//           const rowData = headers.map(header => {
+//             const value = row[header];
+//             if (typeof value === 'string') {
+//               return value.replace(/,/g, '');
+//             }
+//             return String(value);
+//           });
+//           csvContent += rowData.join(',') + '\n';
+//         });
   
-        // Create and download CSV file
-        const csvDataUri = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvContent);
-        const csvLink = document.createElement('a');
-        csvLink.href = csvDataUri;
-        csvLink.download = filename;
-        csvLink.style.display = 'none';
-        document.body.appendChild(csvLink);
-        csvLink.click();
-        document.body.removeChild(csvLink);
+//         // Create and download CSV file
+//         const csvDataUri = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvContent);
+//         const csvLink = document.createElement('a');
+//         csvLink.href = csvDataUri;
+//         csvLink.download = filename;
+//         csvLink.style.display = 'none';
+//         document.body.appendChild(csvLink);
+//         csvLink.click();
+//         document.body.removeChild(csvLink);
   
-        // Create and download HTML file
-        const htmlContent = `<table><thead><tr>${headers.map(header => `<th>${header}</th>`).join('')}</tr></thead><tbody>${data.map(row => `<tr>${headers.map(header => `<td>${row[header]}</td>`).join('')}</tr>`).join('')}</tbody></table>`;
-        const htmlDataUri = 'data:text/html;charset=utf-8,' + encodeURIComponent(htmlContent);
-        const htmlLink = document.createElement('a');
-        htmlLink.href = htmlDataUri;
-        htmlLink.download = `table_version_${version}.html`;
-        htmlLink.style.display = 'none';
-        document.body.appendChild(htmlLink);
-        htmlLink.click();
-        document.body.removeChild(htmlLink);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
+//         // Create and download HTML file
+//         const htmlContent = `<table><thead><tr>${headers.map(header => `<th>${header}</th>`).join('')}</tr></thead><tbody>${data.map(row => `<tr>${headers.map(header => `<td>${row[header]}</td>`).join('')}</tr>`).join('')}</tbody></table>`;
+//         const htmlDataUri = 'data:text/html;charset=utf-8,' + encodeURIComponent(htmlContent);
+//         const htmlLink = document.createElement('a');
+//         htmlLink.href = htmlDataUri;
+//         htmlLink.download = `table_version_${version}.html`;
+//         htmlLink.style.display = 'none';
+//         document.body.appendChild(htmlLink);
+//         htmlLink.click();
+//         document.body.removeChild(htmlLink);
+//       })
+//       .catch(error => {
+//         console.error('Error:', error);
+//       });
+//   }
 
