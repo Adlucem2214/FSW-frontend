d2b93f3b654f688abf12e117824c2c90150c8d5f
sifenbeshada <sefanrobi@gmail.com>
2024-01-31 12:21:18 +0300

    added dashboard js

--- src/Fetch/dashboardfetch.js
+++ src/Fetch/dashboardfetch.js
@@ -32,3 +32,50 @@ fetch('http://localhost:8000/stock/products/')
     .catch(error => {
       console.error('Error:', error);
     });
+
+    fetchTableData();
+
+        async function fetchTableData() {
+            try {
+                const response = await fetch('http://localhost:8000/stock/sales/');
+                const data = await response.json();
+
+                // Access the table element
+                const salesTable = document.getElementById('sales-table');
+
+                // Iterate over the data and populate the table
+                data.forEach((row, index) => {
+                    console.log(data)
+                    // Create a new row
+                    const newRow = document.createElement('tr');
+
+                    // Create and populate the cells
+                    const indexCell = document.createElement('td');
+                    indexCell.textContent = index + 1;
+                    newRow.appendChild(indexCell);
+                    indexCell.id = 'indexid'
+
+                    const productNameCell = document.createElement('td');
+                    productNameCell.textContent = row.product;
+                    newRow.appendChild(productNameCell);
+                    productNameCell.id = 'productid'
+
+                    const dateCell = document.createElement('td');
+                    dateCell.textContent = row.date;
+                    newRow.appendChild(dateCell);
+                    dateCell.id = 'dateid'
+
+
+                    const totalSalesCell = document.createElement('td');
+                    totalSalesCell.textContent = row.total_price;
+                    newRow.appendChild(totalSalesCell);
+                    totalSalesCell.id= 'salesid'
+
+
+                    // Append the new row to the table
+                    salesTable.appendChild(newRow);
+                });
+            } catch (error) {
+                console.error('Error fetching table data:', error);
+            }
+        }
